faec1f8d61a14fc6a3c623f87cb1152bae875f51
Brianconn71 <Brian.connolly71@gmail.com>
2021-01-04 16:55:42 +0000

    added github integration

---
+++ script.js
@@ -0,0 +1,32 @@
+const APIURL = 'https://api.github.com/users/'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
+
+async function getUser(username){
+    try{
+        const res = await axios(APIURL + username)
+
+        console.log(res.data)
+
+    } catch (err){
+            console.log(err)
+    }
+    
+}
+
+function createUserCard(user){
+    const cardHtml = ``
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = search.value
+
+    if(user){
+        getUser(user)
+
+        search.value = ''
+    }
+})
